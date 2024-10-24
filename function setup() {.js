a6a01543c8a2495b9dd69f492f11ec9767eef761
VitaoNZZZ <victor.palmieri@escola.pr.gov.br>
2024-10-24 11:18:48 -0300

    Add files via upload

---
+++ function setup() {.js
@@ -0,0 +1,38 @@
+function setup() {
+    createCanvas(400, 400);
+    prompt("VITÃO"); 
+  }
+  
+  function draw() {
+    background(220);
+  }
+  function setup() {
+    createCanvas(400, 400);
+    let palavra = prompt("VITÃO"); 
+  }
+
+  function draw() {
+    background(220);
+  }
+  function draw() {
+    background("#01080E");
+  }
+  function setup() {
+    createCanvas(400, 400);
+    let palavra = prompt("VITÃO")
+    
+      background(#01080E);
+     console.log(palavra)
+  }
+  
+  function draw() {  
+  }
+  function setup() {
+    createCanvas(400, 400);
+    let palavra = prompt("VITÃO"); 
+
+    background("#01080e");
+    console.log(palavra);
+
+    noLoop();
+  }
